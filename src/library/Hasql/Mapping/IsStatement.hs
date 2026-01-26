4f128466aa4fd6cb3cf36ad619fdd9731e157cfd
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-01-26 12:01:27 +0300

    Add docs

--- src/library/Hasql/Mapping/IsStatement.hs
+++ src/library/Hasql/Mapping/IsStatement.hs
@@ -2,6 +2,9 @@ module Hasql.Mapping.IsStatement where
 
 import qualified Hasql.Statement as Statement
 
+-- | Mapping to a SQL statement indexed by its parameter type with result type associated.
+--
+-- Use this to define modular mappings, where each statement is defined in an isolated module
 class IsStatement a where
   type ResultOf a
   statementOf :: Statement.Statement a (ResultOf a)
