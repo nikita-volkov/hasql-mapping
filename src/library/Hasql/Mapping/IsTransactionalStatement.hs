36d4068c66fc49976c81300c3abd0730950fb72e
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-02-04 15:53:44 +0300

    Drop the IsTransactionalStatement module for now

--- src/library/Hasql/Mapping/IsTransactionalStatement.hs
+++
@@ -1,9 +0,0 @@
-module Hasql.Mapping.IsTransactionalStatement where
-
-import Data.Tagged (Tagged (..))
-import Hasql.Mapping.IsStatement
-import Prelude
-
--- | A statement that has transactional properties associated with it.
-class (IsStatement a) => IsTransactionalStatement a where
-  writes :: Tagged a Bool
